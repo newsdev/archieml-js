8a3b24b4a579aa473c5ea75a7a4ab71500da4635
Gregor Aisch <gregor.aisch@nytimes.com>
2015-06-25 09:43:46 -0400

    added base structure for vows test suite

---
+++ test/parse-documents-test.js
@@ -0,0 +1,26 @@
+
+var vows = require('vows'),
+    archieml = require('../archieml'),
+    assert = require('assert'),
+    fs = require('fs'),
+    glob = require('glob');
+
+var tests = {};
+
+glob.sync(__dirname + '/test-documents/*.aml').forEach(function(f) {
+
+    var key = f.substring(f.lastIndexOf('/')+1, f.length-4), a,b;
+
+    tests[key] = {
+        topic: function() { return archieml.load(fs.readFileSync(f, 'utf-8')); },
+        'matches json': function(topic) {
+            var expected = JSON.parse(fs.readFileSync(f.replace('.aml', '.json'), 'utf-8'));
+            assert.deepEqual(topic, expected);
+        }
+    };
+
+});
+
+vows.describe('Parsing a bunch of test documents')
+    .addBatch(tests)
+    .exportTo(module);
